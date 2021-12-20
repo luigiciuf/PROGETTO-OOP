6986431c054c99497ebb155fa42838a5e5b1ac07
ciuffreda_luigi <92534853+luigiciuf@users.noreply.github.com>
2021-12-20 11:25:50 +0100

    creazione controller

---
+++ SpringAPP/SpringAPP/src/main/java/com/progettounivpm/SpringAPP/controller/Controller.java
@@ -0,0 +1,24 @@
+package com.progettounivpm.SpringAPP.controller;
+
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.progettounivpm.SpringAPP.service.TwitterServiceImpl;
+public class Controller{
+	
+}
+
+	
+
+	
+	
+	
+		
+
+
