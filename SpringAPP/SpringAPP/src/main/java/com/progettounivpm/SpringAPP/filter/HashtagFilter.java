306272cff99c79339001465ee77ed2bd4d07a440
Fede-Rosso <95428877+Fede-Rosso@users.noreply.github.com>
2021-12-21 01:48:37 +0100

    Parziale sviluppo filtro per hashtag

--- SpringAPP/SpringAPP/src/main/java/com/progettounivpm/SpringAPP/filter/HashtagFilter.java
+++ SpringAPP/SpringAPP/src/main/java/com/progettounivpm/SpringAPP/filter/HashtagFilter.java
@@ -1,5 +1,41 @@
 package com.progettounivpm.SpringAPP.filter;
 
-public class HashtagFilter {
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import com.progettounivpm.SpringAPP.model.Tweet;
+
+public class HashtagFilter implements Filter{
+	private String 	hashtag;
+	private ArrayList<Tweet> tweets;
+
+	//Costruttore
+	public HashtagFilter(String hashtag, ArrayList<Tweet> tweets) {
+		this.hashtag = hashtag;
+		this.tweets = tweets;
+	}
+	
+	@Override
+	public JSONObject filter() {
+		
+		JSONObject oggettoFiltrato = new JSONObject();
+		JSONArray jarray = new JSONArray();
+		for (Tweet t: tweets) {
+			JSONObject tweet = null;
+			ArrayList<String> hashtags = t.getHashtags();
+			for (String h: hashtags) {
+				if (h.equals(hashtag)) {
+					tweet.put("created_at",t.getCreated_at_Text());
+					// da continuare
+				}
+				if(tweet != null) {
+					jarray.add(tweet);
+					break; // per evitare che continui a cercare nuovamente tra gli hashtag
+				}
+			}
+			oggettoFiltrato.put("tweets with hashtag " + hashtag, jarray);
+		}	
+		return oggettoFiltrato;
+	}
 
 }
