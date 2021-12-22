e526ad41c87f8be69e453cd82567e1c29c8f3631
ciuffreda_luigi <92534853+luigiciuf@users.noreply.github.com>
2021-12-22 19:29:03 +0100

    Update Statistic.java

--- SpringAPP/SpringAPP/src/main/java/com/progettounivpm/SpringAPP/statistics/Statistic.java
+++ SpringAPP/SpringAPP/src/main/java/com/progettounivpm/SpringAPP/statistics/Statistic.java
@@ -1,10 +1,13 @@
 package com.progettounivpm.SpringAPP.statistics;
 
+
+
 import java.util.HashMap;
 
 import org.json.simple.JSONObject;
 
 public interface Statistic {
 	public JSONObject Statistic();
+	public HashMap Statistic1();
 
 }
