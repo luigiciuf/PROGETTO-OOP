72dfa3a0899eb8bc452eeb13db826439768670e6
Fede-Rosso <fede.rossolini@gmail.com>
2021-12-26 17:09:22 +0100

    correzione errore ParseException

--- SpringAPP/SpringAPP/src/main/java/com/progettounivpm/SpringAPP/service/TwitterServiceImpl.java
+++ SpringAPP/SpringAPP/src/main/java/com/progettounivpm/SpringAPP/service/TwitterServiceImpl.java
@@ -6,15 +6,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.text.ParseException;
 import java.util.ArrayList;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
-//import org.json.simple.parser.ParseException;
+import org.json.simple.parser.ParseException;
 
-import org.springframework.core.codec.AbstractDataBufferDecoder;
 import org.springframework.stereotype.Service;
 
 import com.progettounivpm.SpringAPP.model.Tweet;
@@ -22,10 +20,9 @@ import com.progettounivpm.SpringAPP.model.Tweet;
 @Service 
 // deve implementare tutti i metodi usati sull'interfaccia che eredita
 public class TwitterServiceImpl implements TwitterService{
-	
-	
+
 	private String url= "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=";
-	//private String ApiKey = ""; // TODO: Da implementare poi quando sostituiremo il url
+	//private String ApiKey = ""; TODO: Da implementare poi quando sostituiremo il url
 	
 	@Override
 	public JSONObject getJSONTweets(String hashtag,int count) throws IOException {
@@ -33,11 +30,6 @@ public class TwitterServiceImpl implements TwitterService{
 		JSONObject tweets = null; //oggetto locale che servirà per il return
 		
 		URLConnection openConnection= new URL( url + hashtag + "&count=" + count).openConnection(); // Open connection crea la connessione con il URL indicato
-		/*POSSIBILE IMPLEMENTAZIONE:
-		 *	int count; //stabilisce quanti tweet cercare
-		 *  new URL( url + hastag + "&count=" + conunt ).openConnection()
-		 */
-		
 		// Volendo è possibile ridefinire openConnection come lettura di un file json o txt da un percorso stabilito
 		
 		InputStream in = openConnection.getInputStream();  //legge la connessione creando un oggetto in di tipo InputStream
@@ -53,11 +45,9 @@ public class TwitterServiceImpl implements TwitterService{
 			// dopo aver salvato tutto quello letto su data facciamo un cast di tipo JSONObject e salviamo sull'oggetto che restituiremo
 			tweets = (JSONObject)JSONValue.parseWithException(data);	
 		}
-		/* TODO: ERRORE
 		catch (ParseException e) {
 			e.printStackTrace();
 		}
-		*/
 		catch (Exception e) {
 			e.printStackTrace();
 		}
