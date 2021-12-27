ed7c0aabcb2a8ae74ec3b89c0e281c9ac266e012
ciuffreda_luigi <92534853+luigiciuf@users.noreply.github.com>
2021-12-27 12:37:24 +0100

    Create Tests.java

---
+++ SpringAPP/SpringAPP/src/test/java/com/progettounivpm/SpringAPP/Tests.java
@@ -0,0 +1,41 @@
+package com.progettounivpm.SpringAPP;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.progettounivpm.SpringAPP.model.DataOra;
+import com.progettounivpm.SpringAPP.model.Tweet;
+import com.progettounivpm.SpringAPP.service.TwitterServiceImpl;
+
+@SpringBootTest
+public class Tests {
+	
+	
+	@BeforeEach
+	void initialization() throws Exception{
+		
+		
+		
+		
+	}
+	
+	
+	@Test
+	@DisplayName()
+
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+	
+	
+
+}
