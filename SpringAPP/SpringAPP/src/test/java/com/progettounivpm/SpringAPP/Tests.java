66ea4a049489757fec73753d8e9fd766aff0ca98
ciuffreda_luigi <92534853+luigiciuf@users.noreply.github.com>
2021-12-29 17:36:54 +0100

    Update Tests.java

--- SpringAPP/SpringAPP/src/test/java/com/progettounivpm/SpringAPP/Tests.java
+++ SpringAPP/SpringAPP/src/test/java/com/progettounivpm/SpringAPP/Tests.java
@@ -1,29 +1,105 @@
 package com.progettounivpm.SpringAPP;
 
+import java.util.ArrayList;
+
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import com.progettounivpm.SpringAPP.model.DataOra;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.progettounivpm.SpringAPP.filter.HashtagFilter;
+
 import com.progettounivpm.SpringAPP.model.Tweet;
 import com.progettounivpm.SpringAPP.service.TwitterServiceImpl;
 
 @SpringBootTest
 public class Tests {
+	ArrayList<Tweet> diprova= null;
+	org.json.simple.JSONObject allTweetsData;
+	TwitterServiceImpl downloadTweets = new TwitterServiceImpl();
 	
 	
-	@BeforeEach
-	void initialization() throws Exception{
+	
+	
+	
+	@BeforeEach 
+	void inizializzazione() throws Exception{
+	
+		
+		allTweetsData = downloadTweets.getJSONTweets("univpm",100);
+		diprova = downloadTweets.getTweetInfo(allTweetsData);
+		//String percorso = " C:\\Users\\luigi\\Desktop\\FILE RESTITUITI\\ prima chiamata.json";
+		//diprova= downloadTweets.readFile( "primachiamata.json");
+		
+		
+	}
+	
+	// assertArrayEquals(Object[] expecteds, Object[] actuals)
+   // Asserts that two object arrays are equal.
+	
+	@Test 
+	@DisplayName ("Controllo chiamata API e parsing")
+	void test1() throws Exception{
+		//siccome non possiamo usare assert con un arraylist facciamo un ciclo per controllare l'avvenuta del parsing
+		for(Tweet t : diprova ) {
+			Assert.notNull(t);
+			//System.out.println(t); verifa corretto funzionamento
+			
+			
+			
+			
+		}
+	}
+	
+	@Test
+	@DisplayName("verifica filtro con zero risultati")
+	void test2()throws Exception{
+		String hashtag = "djcsDVCScdccas";
+		JSONArray JarrayTweets= new JSONArray();
+		JSONObject outputdesiderato = new JSONObject();
+		outputdesiderato.put("Non ci sono tweets con hashtag: " + hashtag, JarrayTweets);
+		HashtagFilter filtroprova= new HashtagFilter(hashtag, diprova);
+		JSONObject output= new JSONObject();
+		output=filtroprova.filter();
+		System.out.println(output);
+		System.out.println(outputdesiderato);
+		assertEquals(output,outputdesiderato);
+		
+		
+		
+		
+
+		
+	
+	}
+	
+
+		
 		
 		
 		
 		
 	}
+
+
+
 	
+
+
+
 	
-	@Test
-	@DisplayName()
+
+
+
+
 
 		
 		
@@ -32,10 +108,25 @@ public class Tests {
 		
 		
 		
-		}
+		
+		
+	
+	
+	
+
+
+
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 	
 	
 
-}
+
