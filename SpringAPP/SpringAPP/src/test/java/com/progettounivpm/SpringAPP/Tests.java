7bfe1561ab4b31520c74b2ef06cfef42493aeaed
Fede-Rosso <fede.rossolini@gmail.com>
2021-12-29 18:27:20 +0100

    aggiunta commenti Test

--- SpringAPP/SpringAPP/src/test/java/com/progettounivpm/SpringAPP/Tests.java
+++ SpringAPP/SpringAPP/src/test/java/com/progettounivpm/SpringAPP/Tests.java
@@ -1,9 +1,6 @@
 package com.progettounivpm.SpringAPP;
 
 import java.util.ArrayList;
-
-
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,121 +9,66 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.progettounivpm.SpringAPP.filter.HashtagFilter;
-
 import com.progettounivpm.SpringAPP.model.Tweet;
 import com.progettounivpm.SpringAPP.service.TwitterServiceImpl;
 
 @SpringBootTest
 public class Tests {
 	ArrayList<Tweet> diprova= null;
-	org.json.simple.JSONObject allTweetsData;
+	JSONObject allTweetsData;
 	TwitterServiceImpl downloadTweets = new TwitterServiceImpl();
-	
-	
-	
 	
 	
 	@BeforeEach 
 	void inizializzazione() throws Exception{
-	
-		
+		//Costruzione oggetto con il link come variabile interna e con i metodi per la chiamata API e PARSING.
 		allTweetsData = downloadTweets.getJSONTweets("univpm",100);
+		//Costruzione ArrayList con i Tweet
 		diprova = downloadTweets.getTweetInfo(allTweetsData);
-		//String percorso = " C:\\Users\\luigi\\Desktop\\FILE RESTITUITI\\ prima chiamata.json";
-		//diprova= downloadTweets.readFile( "primachiamata.json");
-		
-		
+		//SOSTITUENDO LA CHIAMATA POSSIAMO USARE UN FILE PER LA LETTURA DEI TWEET
+		//diprova= downloadTweets.readFile("chiamataprova.json");
+		/*
+		System.out.println("ARRAY CREATO:");
+		System.out.println("-------------------------------------------------------------------");
+		System.out.println(diprova);
+		System.out.println("-------------------------------------------------------------------");
+		 */
 	}
 	
-	// assertArrayEquals(Object[] expecteds, Object[] actuals)
-   // Asserts that two object arrays are equal.
+	// assertArrayEquals(Object[] expecteds, Object[] actuals) Asserts that two object arrays are equal.
 	
 	@Test 
 	@DisplayName ("Controllo chiamata API e parsing")
 	void test1() throws Exception{
-		//siccome non possiamo usare assert con un arraylist facciamo un ciclo per controllare l'avvenuta del parsing
+		//Siccome non possiamo usare assert con un arraylist facciamo un ciclo per controllare l'avvenuta del parsing
 		for(Tweet t : diprova ) {
-			Assert.notNull(t);
-			//System.out.println(t); verifa corretto funzionamento
-			
-			
-			
-			
+			//System.out.println(t); Stampa per verifare il corretto funzionamento
+			Assert.notNull(t);		
 		}
 	}
 	
 	@Test
 	@DisplayName("verifica filtro con zero risultati")
 	void test2()throws Exception{
+		//Usiamo un hashtag strano come parametro per il filtro, per ottenere zero risultati 
 		String hashtag = "djcsDVCScdccas";
+		//costruiamo l'output che ci aspettiamo
 		JSONArray JarrayTweets= new JSONArray();
 		JSONObject outputdesiderato = new JSONObject();
 		outputdesiderato.put("Non ci sono tweets con hashtag: " + hashtag, JarrayTweets);
+		//chiamiamo il filtro
 		HashtagFilter filtroprova= new HashtagFilter(hashtag, diprova);
 		JSONObject output= new JSONObject();
 		output=filtroprova.filter();
-		System.out.println(output);
-		System.out.println(outputdesiderato);
+		//TEST
 		assertEquals(output,outputdesiderato);
-		
-		
-		
-		
-
-		
-	
 	}
 	
 
 		
 		
-		
-		
-		
-	}
-
-
-
-	
-
-
-
-	
-
-
-
-
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	
-	
-	
-
-
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	
-	
-
-
+			
+}
